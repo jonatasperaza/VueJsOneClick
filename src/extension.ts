e1288c66dcc7859ee591d0875e216b822cfa0f9d
Jonatas Silva peraza <jonatassilvaperaza@gmail.com>
2023-10-20 22:20:13 -0300

    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,24 @@ import { promisify } from 'util';
 import { spawnSync } from 'child_process';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as os from 'os';
+import { exec } from 'child_process';
 
-const exec = promisify(require('child_process').exec);
+const execAsync = promisify(exec);
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Vue One-Click extension is active.');
 
+    // Create a status bar item and add it to the status bar
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    statusBarItem.text = "$(rocket) Vue One-Click";
+    statusBarItem.tooltip = "Create Vue Project";
+    statusBarItem.command = 'vueOneClick.install';
+    statusBarItem.show();
+
+    context.subscriptions.push(statusBarItem);
+
+    // Register a command to be executed when the status bar item is clicked
     const disposable = vscode.commands.registerCommand('vueOneClick.install', async () => {
         try {
             if (!isNpmInstalled()) {
@@ -22,13 +34,25 @@ export function activate(context: vscode.ExtensionContext) {
             });
 
             if (!projectFolderName) {
-                return; // O usuário cancelou a entrada.
+                return; // The user canceled the input.
             }
 
-            if (await installVueCLI()) {
-                if (await createVueProject(projectFolderName)) {
-                    vscode.window.showInformationMessage(`Vue.js project '${projectFolderName}' has been created.`);
+            if (os.platform() === 'win32') {
+                // Execute Windows-specific commands
+                if (await installVueCLIWindows()) {
+                    if (await createVueProjectWindows(projectFolderName)) {
+                        vscode.window.showInformationMessage(`Vue.js project '${projectFolderName}' has been created.`);
+                    }
                 }
+            } else if (os.platform() === 'linux') {
+                // Execute Linux-specific commands
+                if (await installVueCLILinux()) {
+                    if (await createVueProjectLinux(projectFolderName)) {
+                        vscode.window.showInformationMessage(`Vue.js project '${projectFolderName}' has been created.`);
+                    }
+                }
+            } else {
+                vscode.window.showErrorMessage('Unsupported platform. This extension supports Windows and Linux only.');
             }
         } catch (error: any) {
             vscode.window.showErrorMessage('Error: ' + error.message);
@@ -49,27 +73,72 @@ function isNpmInstalled(): boolean {
     }
 }
 
-async function installVueCLI(): Promise<boolean> {
+async function installVueCLIWindows(): Promise<boolean> {
     try {
-        vscode.window.showInformationMessage('Vue CLI is being installed.');
-        await exec('npm install -g @vue/cli');
-        vscode.window.showInformationMessage('Vue CLI has been installed successfully.');
+        vscode.window.showInformationMessage('Vue CLI is being installed on Windows.');
+        // Execute Windows-specific npm installation command
+        const { stdout, stderr } = await execAsync('npm install -g @vue/cli');
+        if (stderr) {
+            throw new Error(stderr);
+        }
+        vscode.window.showInformationMessage('Vue CLI has been installed on Windows successfully.');
         return true;
     } catch (error: any) {
-        vscode.window.showErrorMessage('Error installing Vue CLI via npm: ' + error.message);
+        vscode.window.showErrorMessage('Error installing Vue CLI on Windows via npm: ' + error.message);
         return false;
     }
 }
 
-async function createVueProject(projectFolderName: string): Promise<boolean> {
+async function createVueProjectWindows(projectFolderName: string): Promise<boolean> {
     const projectFolderPath = path.join(vscode.workspace.rootPath || '', projectFolderName);
-    
+
     try {
-        vscode.window.showInformationMessage('Creating Vue.js project...');
-        await exec(`vue create --preset default ${projectFolderName}`, { cwd: vscode.workspace.rootPath });
+        vscode.window.showInformationMessage('Creating Vue.js project on Windows...');
+        // Execute Windows-specific Vue project creation command
+        const { stdout, stderr } = await execAsync(`vue create --preset default ${projectFolderName}`, {
+            cwd: vscode.workspace.rootPath
+        });
+        if (stderr) {
+            throw new Error(stderr);
+        }
         return true;
     } catch (error: any) {
-        vscode.window.showErrorMessage('Error creating the Vue.js project: ' + error.message);
+        vscode.window.showErrorMessage('Error creating the Vue.js project on Windows: ' + error.message);
+        return false;
+    }
+}
+
+async function installVueCLILinux(): Promise<boolean> {
+    try {
+        vscode.window.showInformationMessage('Vue CLI is being installed on Linux.');
+        // Execute Linux-specific command with sudo-prompt
+        const { stdout, stderr } = await execAsync('npm install -g @vue/cli');
+        if (stderr) {
+            throw new Error(stderr);
+        }
+        vscode.window.showInformationMessage('Vue CLI has been installed on Linux successfully.');
+        return true;
+    } catch (error: any) {
+        vscode.window.showErrorMessage('Error installing Vue CLI on Linux via npm: ' + error.message);
+        return false;
+    }
+}
+
+async function createVueProjectLinux(projectFolderName: string): Promise<boolean> {
+    const projectFolderPath = path.join(vscode.workspace.rootPath || '', projectFolderName);
+
+    try {
+        vscode.window.showInformationMessage('Creating Vue.js project on Linux...');
+        // Execute Linux-specific Vue project creation command with sudo-prompt
+        const { stdout, stderr } = await execAsync(`vue create --preset default ${projectFolderName}`, {
+            cwd: vscode.workspace.rootPath
+        });
+        if (stderr) {
+            throw new Error(stderr);
+        }
+        return true;
+    } catch (error: any) {
+        vscode.window.showErrorMessage('Error creating the Vue.js project on Linux: ' + error.message);
         return false;
     }
 }
